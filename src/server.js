12b0e24008f86ba5dbbde3175b5ad118d3a653a1
Aleydon <aleydonroberto@gmail.com>
2021-04-13 00:26:56 -0300

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const server = express();
+const PORT = process.env.PORT || 3333;
+
+server.use(cors());
+server.use(express.json());
+
+server.listen(PORT, (req, res) =>
+  console.log(`Server running on port ${PORT}`)
+);

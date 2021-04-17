d17797e21855780ae4f46c34eb31f5ab8d1ce26c
Aleydon <aleydonroberto@gmail.com>
2021-04-17 16:31:09 -0300

    Config  database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'aleydon123',
+  database: 'tasklist',
+  define: {
+    timestamps: true,
+    underscored: true,
+    undescoredAll: true
+  }
+};

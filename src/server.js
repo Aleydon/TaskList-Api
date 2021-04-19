ce8e48fa6768dfd0438ea5acb0db24bbef176d9d
Aleydon <aleydonroberto@gmail.com>
2021-04-19 18:55:35 -0300

    Added Routes to server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 
+import Routes from './routes';
 import './database';
 
 const server = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 3333;
 
 server.use(cors());
 server.use(express.json());
+server.use(Routes);
 
 server.listen(PORT, (req, res) =>
   console.log(`Server running on port ${PORT}`)
